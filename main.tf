6977578f7de91062731992609a8b504296d8f3a8
RekhuGopal <42715745+RekhuGopal@users.noreply.github.com>
2020-12-26 16:51:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-# ARM provider block
+#ARM provider block
 provider "azurerm" {
   version = "~>2.0"
   features {}
 }
 
-# Terraform configuration block
+#Terraform configuration block
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-cloudquickpocs"
@@ -14,7 +14,7 @@ terraform {
   }
 }
 
-# RG creation
+#RG creation
 resource "azurerm_resource_group" "rg-hello-azure" {
   name     = "rg-cloudquickpocs"
   location = "northeurope"
