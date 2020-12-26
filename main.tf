e0ec21aab0c7e5949b1628ba1d0a642a0e92c63f
RekhuGopal <42715745+RekhuGopal@users.noreply.github.com>
2020-12-26 16:45:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
+# ARM provider block
 provider "azurerm" {
   version = "~>2.0"
   features {}
 }
 
+# Terraform configuration block
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-hello-azure-tf"
-    storage_account_name = "sahelloazuretf"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "rg-cloudquickpocs"
+    storage_account_name = "ccpsazuretf0001"
+    container_name       = "ccpterraformstatefile"
+    key                  = "ccpsterraform.tfstate"
   }
 }
 
+# RG creation
 resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
-  location = "northcentralus"
+  name     = "rg-cloudquickpocs"
+  location = "northeurope"
 }
