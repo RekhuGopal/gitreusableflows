c141fffedc61fae320fe5ede584aa5853a3732b2
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-01 15:21:38 +0530

    change-37

--- createsqldatabase.tf
+++ createsqldatabase.tf
@@ -61,7 +61,7 @@ resource "azurerm_template_deployment" "failovergroup" {
   name                = "failover"
   resource_group_name = "${azurerm_resource_group.main.*.name[0]}"
   template_body       = "${file("Template/failover.json")}"
-  deployment_mode     = "Incremental"
+
 
   parameters {
     sqlServerPrimaryName  = "${azurerm_sql_server.main.*.name[0]}"
@@ -70,4 +70,6 @@ resource "azurerm_template_deployment" "failovergroup" {
     partnerServers        = "${join(",", slice(azurerm_sql_server.main.*.name, 1, length(var.regions)))}"
     partnerResourceGroups = "${join(",", slice(azurerm_resource_group.main.*.name, 1, length(var.regions)))}"
   }
+
+  deployment_mode     = "Incremental"
 }
