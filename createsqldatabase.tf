cf636501eccaad2950fce225fb7738cec05df4a7
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-01 15:25:13 +0530

    Change-38

--- createsqldatabase.tf
+++ createsqldatabase.tf
@@ -1,51 +1,44 @@
-
-# variable "local"
 variable "regions" {
   type        = "list"
   default     = ["East US"]
+  description = "Regions to deploy your resources, can be one or more. Defaults to East US and West US."
 }
 
-# variable "locals"
 locals {
   tags = {
-    "managed"     = "via IaC"
-    "owner"       = "quickcloudpocs"
-    "environment" = "pocs"
+    "managed"     = "By IaC"
+    "owner"       = "quickCloudPocs"
+    "environment" = "POC"
   }
 }
 
-# create a RG
 resource "azurerm_resource_group" "main" {
-  count    = "${length(var.regions)}"
   name     = "MyDB-RG-${count.index}"
   location = "${element(var.regions, count.index)}"
   tags     = "${local.tags}"
+  count    = "${length(var.regions)}"
 }
 
-
-# Create a SQL server for DB
 resource "azurerm_sql_server" "main" {
-  count                        = "${length(var.regions)}"
   name                         = "mytfqlserver-${count.index}"
   resource_group_name          = "${element(azurerm_resource_group.main.*.name, count.index)}"
   location                     = "${element(azurerm_resource_group.main.*.location, count.index)}"
   version                      = "12.0"
-  administrator_login          = "xyz0001"
-  administrator_login_password = "mysupersecretepaass001"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
   tags                         = "${local.tags}"
+  count                        = "${length(var.regions)}"
 }
 
-# create firewall rules
 resource "azurerm_sql_firewall_rule" "main" {
-  count               = "${length(var.regions)}"
   name                = "AllowAzureServices"
   resource_group_name = "${element(azurerm_resource_group.main.*.name, count.index)}"
   server_name         = "${element(azurerm_sql_server.main.*.name, count.index)}"
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
+  count               = "${length(var.regions)}"
 }
 
-# Create SQL DB
 resource "azurerm_sql_database" "main" {
   name                             = "mysqldatabase"
   resource_group_name              = "${azurerm_resource_group.main.*.name[0]}"
@@ -56,20 +49,19 @@ resource "azurerm_sql_database" "main" {
   tags                             = "${local.tags}"
 }
 
-# Azure Resource Group deployment SQL server failover group
 resource "azurerm_template_deployment" "failovergroup" {
   name                = "failover"
   resource_group_name = "${azurerm_resource_group.main.*.name[0]}"
-  template_body       = "${file("Template/failover.json")}"
 
+  template_body = "${file("arm/failover.json")}"
 
   parameters {
-    sqlServerPrimaryName  = "${azurerm_sql_server.main.*.name[0]}"
-    sqlDatabaseName       = "${azurerm_sql_database.main.name}"
-    sqlFailoverGroupName  = "myfailover"
-    partnerServers        = "${join(",", slice(azurerm_sql_server.main.*.name, 1, length(var.regions)))}"
-    partnerResourceGroups = "${join(",", slice(azurerm_resource_group.main.*.name, 1, length(var.regions)))}"
+    "sqlServerPrimaryName"  = "${azurerm_sql_server.main.*.name[0]}"
+    "sqlDatabaseName"       = "${azurerm_sql_database.main.name}"
+    "sqlFailoverGroupName"  = "myfailover"
+    "partnerServers"        = "${join(",", slice(azurerm_sql_server.main.*.name, 1, length(var.regions)))}"
+    "partnerResourceGroups" = "${join(",", slice(azurerm_resource_group.main.*.name, 1, length(var.regions)))}"
   }
 
-  deployment_mode     = "Incremental"
+  deployment_mode = "Incremental"
 }
