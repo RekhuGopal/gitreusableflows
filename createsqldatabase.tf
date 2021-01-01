f36809510b103f9d9f04df0eab76a44d6455f8ba
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-01 15:45:23 +0530

    change-44

--- createsqldatabase.tf
+++ createsqldatabase.tf
@@ -14,14 +14,15 @@ locals {
 
 # Creates RG dynamically
 resource "azurerm_resource_group" "main" {
+  count    = "${length(var.regions)}"
   name     = "MyDB-RG-${count.index}"
   location = "${element(var.regions, count.index)}"
   tags     = "${local.tags}"
-  count    = "${length(var.regions)}"
 }
 
 # Creates SQL Server
 resource "azurerm_sql_server" "main" {
+  count                        = "${length(var.regions)}"
   name                         = "mytfqlserver-${count.index}"
   resource_group_name          = "${element(azurerm_resource_group.main.*.name, count.index)}"
   location                     = "${element(azurerm_resource_group.main.*.location, count.index)}"
@@ -29,17 +30,16 @@ resource "azurerm_sql_server" "main" {
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
   tags                         = "${local.tags}"
-  count                        = "${length(var.regions)}"
 }
 
 # Creates SQL Server firewall
 resource "azurerm_sql_firewall_rule" "main" {
+  count               = "${length(var.regions)}"
   name                = "AllowAzureServices"
   resource_group_name = "${element(azurerm_resource_group.main.*.name, count.index)}"
   server_name         = "${element(azurerm_sql_server.main.*.name, count.index)}"
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
-  count               = "${length(var.regions)}"
 }
 
 # Creates SQL DB
@@ -57,8 +57,8 @@ resource "azurerm_sql_database" "main" {
 resource "azurerm_template_deployment" "failovergroup" {
   name                = "failover"
   resource_group_name = "${azurerm_resource_group.main.*.name[0]}"
-
   template_body = "${file("Template/failover.json")}"
+  deployment_mode = "Incremental"
 
   parameters = {
       "sqlServerPrimaryName"  = "${azurerm_sql_server.main.*.name[0]}"
@@ -67,6 +67,4 @@ resource "azurerm_template_deployment" "failovergroup" {
       "partnerServers"        = "${join(",", slice(azurerm_sql_server.main.*.name, 1, length(var.regions)))}"
       "partnerResourceGroups" = "${join(",", slice(azurerm_resource_group.main.*.name, 1, length(var.regions)))}"
   }
-
-  deployment_mode = "Incremental"
 }
