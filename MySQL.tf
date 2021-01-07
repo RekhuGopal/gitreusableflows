d924a53cede07c59426bbf95cd051dfbf9b7bb3e
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-07 22:20:58 +0530

    change-80

--- MySQL.tf
+++ MySQL.tf
@@ -6,43 +6,39 @@ resource "azurerm_resource_group" "mysql" {
 
 # My SQL Server
 resource "azurerm_mysql_server" "mysql" {
-  name                = "cloudquickpocs-mysqlsvr"
-  location            = "${azurerm_resource_group.mysql.location}"
-  resource_group_name = "${azurerm_resource_group.mysql.name}"
+  name                = "cloudquickpoc-mysqlserver"
+  location            = azurerm_resource_group.mysql.location
+  resource_group_name = azurerm_resource_group.mysql.name
 
-  sku {
-    name     = "B_Gen5_2"
-    capacity = "2"
-    tier     = "Basic"
-    family   = "Gen5"
-  }
+  administrator_login          = "mysqladmin"
+  administrator_login_password = "Wegd$##@#!bdv888sa"
 
-  storage_profile {
-    storage_mb            = "5120"
-    backup_retention_days = "7"
-    geo_redundant_backup  = "Disabled"
-  }
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
 
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "@1fhdj245fBBBafs"
-  version                      = "5.7"
-  ssl_enforcement              = "Enabled"
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = true
+  infrastructure_encryption_enabled = true
+  public_network_access_enabled     = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 # mysql DB
 resource "azurerm_mysql_database" "mysql" {
-  name                = "mysqldb001"
-  resource_group_name = "${azurerm_resource_group.mysql.name}"
-  server_name         = "${azurerm_mysql_server.mysql.name}"
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.mysql.name
+  server_name         = azurerm_mysql_server.mysql.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
-
 # my sql firewall
 resource "azurerm_mysql_firewall_rule" "mysql" {
   name                = "cloudquickpocmysql-fwrules"
-  resource_group_name = "${azurerm_resource_group.mysql.name}"
-  server_name         = "${azurerm_mysql_server.mysql.name}"
+  resource_group_name = azurerm_resource_group.mysql.name
+  server_name         = azurerm_mysql_server.mysql.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
