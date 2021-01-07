85120d23e0d3c0f2aac0b0f68c5096b3a15db2bf
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-07 22:58:21 +0530

    change-87

--- MySQL.tf
+++ MySQL.tf
@@ -19,7 +19,6 @@ resource "azurerm_mysql_server" "mysql" {
 
   auto_grow_enabled                 = true
   backup_retention_days             = 7
-  geo_redundant_backup_enabled      = true
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
