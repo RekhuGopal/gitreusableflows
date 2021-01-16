e93fe2eba5536ef159c46a7c36e6ed81fad3e24c
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-16 17:43:23 +0530

    updated-6

--- EC2Web.tf
+++ EC2Web.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = "CloudQuickPOCs"
+
+    workspaces {
+      name = "AWS-CloudQuickPOCs"
+    }
+  }
+}
+
 provider "aws" {
   region = "east-west-1"
 }
--- ProviderAndBackends.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "CloudQuickPOCs"
-
-    workspaces {
-      name = "AWS-CloudQuickPOCs"
-    }
-  }
-}
