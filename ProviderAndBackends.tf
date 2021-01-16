c294bec0c1f61a36fa50e2a50b0f9166823beb2d
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-16 17:26:56 +0530

    updated -1

--- ProviderAndBackends.tf
+++ ProviderAndBackends.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "random" {}
+
 terraform {
   required_providers {
     aws = {
@@ -17,8 +23,3 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "random" {}
