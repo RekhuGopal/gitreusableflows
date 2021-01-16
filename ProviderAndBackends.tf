7b880ece39831d7fafed266cc7bfe30d3b92eb22
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-16 17:36:13 +0530

    updated-3

--- ProviderAndBackends.tf
+++ ProviderAndBackends.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
   backend "remote" {
     organization = "CloudQuickPOCs"
 
