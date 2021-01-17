52cf2e7a2089f62cfe850931eb6032fc71451c0c
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-17 13:07:12 +0530

    AWS-3

---
+++ Main.tf
@@ -0,0 +1,14 @@
+terraform {
+  # Terraform version at the time of writing this post
+  required_version = ">= 0.12.24"
+
+  backend "s3" {
+    bucket = "cloudquickpocsbackendtf"
+    key    = "quickcloudpocsbackend.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
