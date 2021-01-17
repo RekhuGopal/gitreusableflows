3184252d63be839e3f19440078ef6c4c49260fb5
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-17 13:52:07 +0530

    AWS-4

--- EC2Web.tf
+++ EC2Web.tf
@@ -3,7 +3,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0be2609ba883822ec"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -27,5 +27,3 @@ resource "aws_security_group" "web-sg" {
 output "web-address" {
   value = "${aws_instance.web.public_dns}:8080"
 }
-
-#
