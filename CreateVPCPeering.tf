378b224d9ee494ff0368d8736a5c6d471f3d6d83
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-24 13:24:15 +0530

    AWS-26

--- CreateVPCPeering.tf
+++ CreateVPCPeering.tf
@@ -1,14 +1,4 @@
-## Create Peering between VPC-1 and VPC-2
-resource "aws_vpc_peering_connection" "vpcpeerigdemo" {
-  peer_owner_id = "357171621133"
-  peer_vpc_id   = aws_vpc.uswestvpccqpoc.id
-  vpc_id        = aws_vpc.useastvpccqpoc.id
-  peer_region   = "us-east-1"
-  
-  tags = {
-    Name = "VPC1-to-VPC2"
-  }
-}
+
 
 ## Create VPC-1
 resource "aws_vpc" "useastvpccqpoc" {
@@ -24,5 +14,29 @@ resource "aws_vpc" "uswestvpccqpoc" {
   cidr_block = "172.16.0.0/16"
   tags = {
     Name = "VPC-2"
+  }
+}
+
+## Create Peering between VPC-1 and VPC-2
+resource "aws_vpc_peering_connection" "vpcpeerigdemo" {
+  peer_owner_id = "357171621133"
+  peer_vpc_id   = aws_vpc.uswestvpccqpoc.id
+  vpc_id        = aws_vpc.useastvpccqpoc.id
+  peer_region   = "us-east-1"
+  auto_accept   = false
+
+  tags = {
+    Name = "VPC1-to-VPC2"
+  }
+}
+
+## Accepter's side of the connection.
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  provider                  =  aws.central
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpcpeerigdemo.id
+  auto_accept               = true
+
+  tags = {
+    Side = "Accepter"
   }
 }
