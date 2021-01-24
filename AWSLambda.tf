27b3eae74e656c3a050ab5af190bf0377ef67906
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-24 15:53:03 +0530

    aws lambdareleas

--- AWSLambda.tf
+++ AWSLambda.tf
@@ -25,14 +25,14 @@ resource "aws_s3_bucket_object" "object" {
 ## IAM role for lambda
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_role"
-  assume_role_policy = "${path.module}/Project/lambda_assume_role_policy.json"
+  assume_role_policy = "${file("Project/lambda_assume_role_policy.json")}"
 }
 
 ## IAM role-policy for lambda
 resource "aws_iam_role_policy" "lambda_policy" {
   name   = "lambda_policy"
   role   = aws_iam_role.lambda_role.id
-  policy = "${path.module}/Project/lambda_policy.json"
+  policy = "${file("Project/lambda_policy.json")}"
 }
 
 
