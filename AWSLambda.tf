82855d52a22487af9f6aa863302af888bbe4f623
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-24 15:19:38 +0530

    aws-lambda-3

--- AWSLambda.tf
+++ AWSLambda.tf
@@ -1,8 +1,8 @@
 ##Zip the function to be run at function App.
 data "archive_file" "init" {
   type        = "zip"
-  source_file = "Project/hello.js"
-  output_path = "hello.zip"
+  source_file = "${path.module}/Project/hello.js"
+  output_path = "${path.module}/hello.zip"
 }
 
 ## S3 Bucket
@@ -19,20 +19,20 @@ resource "aws_s3_bucket" "cqpocsbucket" {
 resource "aws_s3_bucket_object" "object" {
   bucket = aws_s3_bucket.cqpocsbucket.id
   key    = "hello.zip"
-  source = "hello.zip"
+  source = "${path.module}/hello.zip"
 }
 
 ## IAM role for lambda
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda_role"
-  assume_role_policy = file("Project/lambda_assume_role_policy.json")
+  assume_role_policy = file("${path.module}/Project/lambda_assume_role_policy.json")
 }
 
 ## IAM role-policy for lambda
 resource "aws_iam_role_policy" "lambda_policy" {
   name   = "lambda_policy"
   role   = aws_iam_role.lambda_role.id
-  policy = file("Project/lambda_policy.json")
+  policy = file("${path.module}/Project/lambda_policy.json")
 }
 
 
