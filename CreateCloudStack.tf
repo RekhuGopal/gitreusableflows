391265d9d58dc3726df9def51c28aa4b5b7d100b
RekhuGopal <vrchinnarathod@gmail.com>
2021-01-30 22:13:16 +0530

    AWS-61

--- CreateCloudStack.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudformation_stack" "s3staticwebsite" {
-  name = "s3staticwebsite-stack"
-
-  parameters = {
-    BucketName = "quickcloudpoccloudformationdemobucket1"
-  }
-
-  template_body = file("CloudFormationTemplates/S3.yaml")
-}
