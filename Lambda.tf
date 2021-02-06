36caddc8b5b9e4161cadecb1497073a94ecab9c1
RekhuGopal <vrchinnarathod@gmail.com>
2021-02-06 19:54:32 +0530

    AWS-86

---
+++ Lambda.tf
@@ -0,0 +1,73 @@
+## AWS Lambda function
+resource "aws_lambda_function" "results_updates_lambda" {
+    filename         = "${path.module}/Lambda/example.zip"
+    function_name    = "hello_world_example"
+    role             = "${aws_iam_role.lambda_role.arn}"
+    handler          = "example.handler"
+    source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+    runtime          = "nodejs12.x"
+}
+
+## AWS IAM Role
+resource "aws_iam_role" "lambda_role" {
+    name = "LambdaRole"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        }
+    }
+  ]
+}
+EOF
+}
+
+# AWS IAM Role SQS Policy
+resource "aws_iam_role_policy" "lambda_role_sqs_policy" {
+    name = "AllowSQSPermissions"
+    role = aws_iam_role.lambda_role.id
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "sqs:ChangeMessageVisibility",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes",
+        "sqs:ReceiveMessage"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# AWS IAM Role Logs Policy
+resource "aws_iam_role_policy" "lambda_role_logs_policy" {
+    name = "LambdaRolePolicy"
+    role = aws_iam_role.lambda_role.id
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
