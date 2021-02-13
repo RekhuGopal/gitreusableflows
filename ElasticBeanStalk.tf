a372586b5e470faac6eb78ddc477ba0d38860668
RekhuGopal <vrchinnarathod@gmail.com>
2021-02-13 17:41:22 +0530

    EB-2

--- ElasticBeanStalk.tf
+++ ElasticBeanStalk.tf
@@ -5,10 +5,9 @@ resource "aws_elastic_beanstalk_application" "application" {
 
 ## Elastic bean stalk app environment
 resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "my-cqpocsenv-environment"
+  name                = "my-cqpocs-environment"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.1.5 running Python 3.7"
-
+  solution_stack_name = "64bit Amazon Linux 2 v3.0.3 running Python 3.7"
   setting {
         namespace = "aws:autoscaling:launchconfiguration"
         name      = "IamInstanceProfile"
