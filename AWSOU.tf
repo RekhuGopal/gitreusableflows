d78662fc5987dd7566eddfaf9a9eaa8b72ad9bb4
RekhuGopal <vrchinnarathod@gmail.com>
2021-02-14 13:58:09 +0530

    OU-5

--- AWSOU.tf
+++ AWSOU.tf
@@ -18,10 +18,3 @@ resource "aws_organizations_organizational_unit" "prod" {
   name      = "PROD"
   parent_id = data.aws_organizations_organization.cqpocs.roots.0.id
 }
-
-## Move an AWS Account to destination OU
-resource "aws_organizations_account" "example" {
-  email     = "vrchinnarathod@gmail.com"
-  name      = "Rekhu Gopal"
-  parent_id = aws_organizations_organizational_unit.dev.id
-}
