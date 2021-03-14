20ac01323b72ac47c05fa654b6848f5124c808e6
RekhuGopal <vrchinnarathod@gmail.com>
2021-03-14 18:33:55 +0530

    Sagemaker-12

--- terraform/s3_sagemaker.tf
+++ terraform/s3_sagemaker.tf
@@ -19,5 +19,5 @@ resource "aws_s3_bucket" "s3_bucket_1" {
 resource "aws_s3_bucket_object" "s3_fraud_detection_notebook" {
   bucket = aws_s3_bucket.fraud_detection_function_bucket.id
   key    = "fraud-detection-using-machine-learning/1.0/notebooks/sagemaker_fraud_detection.ipynb"
-  source = "${path.module}/source/notebooks/sagemaker_fraud_detection.ipynb"
+  source = "source/notebooks/sagemaker_fraud_detection.ipynb"
 }
