90286e5359e586d836589ce6319336d29f63fe59
RekhuGopal <vrchinnarathod@gmail.com>
2021-05-17 14:31:23 +0530

    Updated

--- CloudTrail.tf
+++ CloudTrail.tf
@@ -6,6 +6,8 @@ resource "aws_cloudwatch_log_group" "awss3bucketloggroups" {
 
 resource "aws_cloudtrail" "awscloudtrailcqpocs" {
   name                          = "tf-based-cloud-trail"
+  s3_bucket_name                = aws_s3_bucket.cqpocs.id
+  s3_key_prefix                 = "cloudtrailkey"
   include_global_service_events = true
   event_selector {
     read_write_type           = "All"
@@ -17,4 +19,40 @@ resource "aws_cloudtrail" "awscloudtrailcqpocs" {
     }
   }
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.awss3bucketloggroups.arn}:*" # CloudTrail requires the Log Stream wildcard
+}
+
+resource "aws_s3_bucket" "cqpocs" {
+  bucket        = "tf-test-trail-bucket-cqpocs"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailAclCheck",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "arn:aws:s3:::tf-test-trail-bucket-cqpocs"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-test-trail-bucket-cqpocs/cloudtrailkey/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
 }
