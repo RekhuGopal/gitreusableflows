068a464f7c2794635e95f50b411207e12532564d
RekhuGopal <vrchinnarathod@gmail.com>
2021-05-17 16:46:57 +0530

    Updated-1

--- CloudTrail.tf
+++ CloudTrail.tf
@@ -57,12 +57,12 @@ resource "aws_cloudtrail" "awscloudtrailcqpocs" {
   s3_bucket_name                = aws_s3_bucket.cqpocs.id
   s3_key_prefix                 = "cloudtrailkey"
   include_global_service_events = true
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.awss3bucketloggroups.id
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.awss3bucketloggroups.arn}"
   event_selector {
     read_write_type           = "All"
     include_management_events = true
   }
- cloud_watch_logs_role_arn = aws_iam_role.cloudtrail_cqpocsrole.arn # CloudTrail requires the Log Stream wildcard
+ cloud_watch_logs_role_arn = "${aws_iam_role.cloudtrail_cqpocsrole.arn}"
 }
 
 resource "aws_s3_bucket" "cqpocs" {
