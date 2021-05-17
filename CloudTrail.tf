afb8e4ebdd3bab0ba1439bcd6410b191aaf44ed2
RekhuGopal <vrchinnarathod@gmail.com>
2021-05-17 16:33:35 +0530

    log groups

--- CloudTrail.tf
+++ CloudTrail.tf
@@ -60,7 +60,7 @@ resource "aws_cloudtrail" "awscloudtrailcqpocs" {
   s3_bucket_name                = aws_s3_bucket.cqpocs.id
   s3_key_prefix                 = "cloudtrailkey"
   include_global_service_events = true
-  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.awscloudtrailcqpocs.arn
+  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.awss3bucketloggroups.arn
   event_selector {
     read_write_type           = "All"
     include_management_events = true
