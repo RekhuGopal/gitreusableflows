160edca75afcbbfff31fda499ceeeb7282cedfda
RekhuGopal <vrchinnarathod@gmail.com>
2021-05-22 17:59:45 +0530

    DynamoDB-2

--- DynamoDB.tf
+++ DynamoDB.tf
@@ -22,6 +22,21 @@ resource "aws_dynamodb_table" "cqpocs-dynamodb-table" {
     type = "N"
   }
 
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "BusinessTitleIndex"
+    hash_key           = "BusinessTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["BusinessId"]
+  }
+
   tags = {
     Name        = "dynamodb-table-1"
     Environment = "production"
