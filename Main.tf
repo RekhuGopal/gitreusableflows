ec696958386abce419f0ac733580985f65f7e285
RekhuGopal <vrchinnarathod@gmail.com>
2022-04-03 16:28:52 +0530

    Backup Updated

--- Main.tf
+++ Main.tf
@@ -18,3 +18,12 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+module "BackupIAM" {
+  source = "./BackupIAM"
+}
+
+module "BackupInfra" {
+  source = "./BackupInfra"
+  depends_on = [module.BackupIAM]
+}
