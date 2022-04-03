35aee2ee9f26230538e009d12463f77aade5f8e4
RekhuGopal <vrchinnarathod@gmail.com>
2022-04-03 14:25:38 +0530

    Removed GitOps

---
+++ Main.tf
@@ -0,0 +1,27 @@
+## backend data for terraform
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+  organization = "CloudQuickPOCs"
+
+    workspaces {
+      name = "AWS-CloudQuickPOCs"
+    }
+  }
+}
+
+## random provider
+provider "random" {}
+
+## Provider us-east-1
+provider "aws" {
+  region = "us-east-1"
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-## README
-FastAPI example.
