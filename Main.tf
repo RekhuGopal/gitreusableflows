ac31150f0341104aa06facc12f58bf2a85d5dfd2
RekhuGopal <vrchinnarathod@gmail.com>
2022-05-27 22:49:04 +0530

    GitHub Managed

--- Main.tf
+++ Main.tf
@@ -18,12 +18,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-#module "BackupIAM" {
-#  source = "./BackupIAM"
-#}
-
-#module "BackupInfra" {
-#  source = "./BackupInfra"
-#  depends_on = [module.BackupIAM]
-#}
