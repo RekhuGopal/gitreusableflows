7772247eb27c420d75ca18b3c2809ee0c62cf081
RekhuGopal <vrchinnarathod@gmail.com>
2022-08-15 14:42:15 +0530

    Azure TF Cloud

--- S3.tf
+++ S3.tf
@@ -1,8 +1,17 @@
-resource "aws_s3_bucket" "testbucket" {
-  bucket = "awseksbackup"
+resource "azurerm_resource_group" "demo" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "StorageAccountDemo" {
+  name                     = "valuestorageaccountforlabs"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
   tags = {
-    "Env": "Dev",
-    "Video": "CloudQuickLabs"
+    video = "azure"
+    channel = "CloudQuickLabs"
   }
 }
