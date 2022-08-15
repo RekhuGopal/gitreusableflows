4ecfd6fd6ffbc04fdf1c75d4f387c1a93d3cf919
RekhuGopal <vrchinnarathod@gmail.com>
2022-08-15 14:47:20 +0530

    storage account name

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "demo" {
 }
 
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "valuestorageaccountforlabs"
+  name                     = "satestant000012"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
