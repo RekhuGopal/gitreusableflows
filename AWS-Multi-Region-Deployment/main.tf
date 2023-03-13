da982f35dd49e7d15444b4bc583123f45794f744
RekhuGopal <vrchinnarathod@gmail.com>
2023-03-13 21:18:50 +0530

    Updated

--- AWS-Multi-Region-Deployment/main.tf
+++ AWS-Multi-Region-Deployment/main.tf
@@ -1,6 +1,6 @@
 module "sns_topics" {
   source = "./SNS"
   aws_region = var.region
-  aws_regions = var.aws_region
+  aws_regions = var.aws_regions
   aws_sns_topic_name = var.aws_sns_topic_name
 }
