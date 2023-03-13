c81948595ad644fa6b76599d09ce478ded71b9c5
RekhuGopal <vrchinnarathod@gmail.com>
2023-03-13 20:58:29 +0530

    Updated

--- AWS-Multi-Region-Deployment/SNS/main.tf
+++ AWS-Multi-Region-Deployment/SNS/main.tf
@@ -1,8 +1,15 @@
+
+provider "aws" {
+  region = var.region
+} 
+
 resource "aws_sns_topic" "example" {
   count          = length(var.aws_regions)
   name           = "${var.aws_sns_topic_name}-${var.aws_regions[count.index]}"
   display_name   = "Multi Region SNS Topic"
-  provider       = aws.regions[var.aws_regions[count.index]]
+  provider = {
+    aws = "aws.${var.aws_regions[count.index]}"
+  }
 }
 
 output "sns_topic_arns" {
--- AWS-Multi-Region-Deployment/main.tf
+++ AWS-Multi-Region-Deployment/main.tf
@@ -1,9 +1,6 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 module "sns_topics" {
   source = "./SNS"
+  aws_region = var.region
   aws_regions = var.aws_region
   aws_sns_topic_name = var.aws_sns_topic_name
 }
