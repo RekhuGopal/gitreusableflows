bf7a8090dcb1745937f649a6a5b6108862e1ef0f
RekhuGopal <vrchinnarathod@gmail.com>
2023-03-15 16:42:56 +0530

    Updated

--- AWS-Multi-Region-Deployment/SNS/variables.tf
+++ AWS-Multi-Region-Deployment/SNS/variables.tf
@@ -2,3 +2,7 @@ variable "aws_sns_topic_name" {
   type    = string
   default = "cloud_quick_labs_sns"
 }
+
+variable "aws_region" {
+  type    = string
+}
--- AWS-Multi-Region-Deployment/dev.tfvars
+++
@@ -1,1 +0,0 @@
-aws_sns_topic_name = "cloud_quick_labs_sns"
