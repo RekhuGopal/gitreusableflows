d90417b87ca1f08f03df08445b3d9b8cf8d6c41b
RekhuGopal <vrchinnarathod@gmail.com>
2023-03-15 17:49:00 +0530

    Updated

--- AWS-Multi-Region-Deployment/SNS/main.tf
+++ AWS-Multi-Region-Deployment/SNS/main.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_sns_topic" "example" {
   name           = "${var.aws_sns_topic_name}-${var.aws_region}"
   display_name   = "Multi Region SNS Topic"
@@ -6,3 +7,4 @@ resource "aws_sns_topic" "example" {
 output "sns_topic_arns" {
   value = aws_sns_topic.example.*.arn
 }
+*/
